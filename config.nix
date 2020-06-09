0d3773b1833be423344c405214a4d9328cf0d75f
Jonathan Ringer <jonringer117@gmail.com>
2020-06-09 12:47:52 -0700

    Add nur

--- config.nix
+++ config.nix
@@ -1,3 +1,11 @@
 {
+  packageOverrides = pkgs: {
+    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+      inherit pkgs;
+    };
+  };
   allowUnfree = true;
+  permittedInsecurePackages = [
+    "p7zip-16.02"
+  ];
 }
