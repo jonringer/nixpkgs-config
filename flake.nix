1e3485dda58bd45e4617a12f893e62d35fecc6f1
Jonathan Ringer <jonringer117@gmail.com>
2022-01-14 18:51:56 -0800

    Use nixpkgs input for home-manager

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, home-manager, nixpkgs, utils }:
